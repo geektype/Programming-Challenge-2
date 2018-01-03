886019edff5065dd4f2f14fd2392d2aa56c155df
geektype <abdullah@softcomnetworks.com>
2018-01-03 14:42:47 +0000

    Added the Python File
    Added funs.py, main.py and the DATA.TXT just in case. But excluded the config.py and test.py as they are not needed

---
+++ funs.py
@@ -0,0 +1,65 @@
+try:
+	from sys import exit
+	from os import system
+except ImportError as e:
+	print("Failed to import 1 or more libraries")
+	print(e)
+	exit()
+def addElements(lst):
+		system('cls')
+		print("Okay, Lets add things to your list!")
+		print("Just keep Entering stuff by Hitting Enter or enter in blank to finish")
+		input("Ready, Click Enter")
+		entering = True
+		while entering:
+			e = input("Enter to add to the list or BLANK to finish")
+			if e == "" or e == "BLANK" or e =="blank":
+				entering = False
+			else:
+				lst.append(e.lower())
+		print(lst)
+		return None
+
+def showByElement(lst):
+	system('cls')
+	ind = int(input("Enter what number Element you want"))
+	print("Your Item is:", " ", lst[ind-1])
+	return None
+
+def showSlice(lst):
+	system('cls')
+	print("You Will be asked to enter the element you want to start from and whether you want stuff before or after it.")
+	idn = int(input("Enter from which element you want to 'slice from:'"))
+	bof = input("Before or After? (B/A)")
+	if bof == "B":
+		print(lst[:idn-1])
+	if bof == "A":
+		print(lst[idn-1:])
+	return None
+def remItem(lst):
+	print("Enter what number item you want to remove")
+	inp = int(input())
+	el = str(lst[inp-1])
+	print("Is this the item you want to remove:", " ", el)
+	conf = input("Y/n")
+	if conf =="n":
+		return None
+	lst.remove(el)
+	print("Item Removed!")
+	input()
+	return None
+def saveToFile(lst):
+	print("I will now try to save the current list to the file 'DATA.txt', make sure it is empty!")
+	try:
+		with open('DATA.txt' , 'w') as f:
+			for i in lst:
+				f.write(i + '\n')
+	except Exception as e:
+		print(e)
+		print("write Failed")
+	print("Write succesful!")
+	return None
+
+
+
+
---
+++ main.py
@@ -0,0 +1,64 @@
+try:
+	from sys import exit
+	from os import system
+except ImportError as e:
+	print("Failed to import 1 or more libraries")
+	print(e)
+	exit()
+try:
+	import funs
+except ImportError as e:
+	print("I failed to import the configuration File (I REALL need it!)")
+	pritn("Make sure there is a config.py and is in this directory!")
+
+
+LIST = ["Hello", "dude", "really", "hope", "you", "are", "good"]
+
+def basicLists():
+
+	if LIST == []:
+		print("You don't have anything in your list, Let's get you started and add Stuff!")
+		input("Hit enter to start!")
+		funs.addElements(LIST)
+	while True:
+		system('cls')
+		print("Here is your current List" + " ", LIST)
+		print("What do you want to do?")
+		print("############################")
+		print("#1.Show by element         #")
+		print("#2.Show a slice            #")
+		print("#3.Remove an Item          #")
+		print("#4.Save your list to file  #")
+		print("#5.Load your list from file#")
+		print("#6.Exit to menu            #")
+		print("############################")
+		choice = int(input())
+		if choice == 1:
+			funs.showByElement(LIST)
+		if choice == 2:
+			funs.showSlice(LIST)
+		if choice == 3:
+			funs.remItem(LIST)
+		if choice == 4:
+			funs.saveToFile(LIST)
+		if choice == 6:
+			return None
+
+		input("")
+while True:
+	system('cls')
+	print("##################")
+	print("#1. Basic Lists  #")
+	print("#2. Quit         #")
+	print("##################")
+	try:
+		choice = int(input("What do you want to do"))
+		if choice == 1:
+			basicLists()
+		elif choice == 2:
+			exit()
+	except ValueError:
+		system('cls')
+		print("What you entered doesn't look like a valid option;)")
+		print("Hit Enter and please try Again!")
+		input()
